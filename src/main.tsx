6c014fa2f226da66f666e7d539445dad8bd9b673
Hridhaan-Sahay <study.hridhaan@gmail.com>
2026-09-11 14:31:39 +0530

    Route BitBuzz landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import LandingPage from "./LandingPage";
 import "./index.css";
+
+const isNewsroom = new URLSearchParams(window.location.search).get("view") === "news";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {isNewsroom ? <App /> : <LandingPage />}
   </StrictMode>,
 );
