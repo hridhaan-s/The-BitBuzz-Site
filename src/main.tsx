d6f56db579086e623ad378c32580ca273c08f37e
Hridhaan-Sahay <study.hridhaan@gmail.com>
2026-09-11 21:37:13 +0530

    Route story submissions through BitBuzz

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import LandingPage from "./LandingPage";
+import SubmitPage from "./SubmitPage";
 import "./index.css";
 
 const url = new URL(window.location.href);
 const isLegacyNewsroom = url.searchParams.get("view") === "news";
 const isHome = url.pathname === "/home";
+const isSubmit = url.pathname === "/submit";
 
 if (isLegacyNewsroom && !isHome) {
   window.history.replaceState({}, "", "/home");
@@ -14,6 +16,6 @@ if (isLegacyNewsroom && !isHome) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {isHome || isLegacyNewsroom ? <App /> : <LandingPage />}
+    {isSubmit ? <SubmitPage /> : isHome || isLegacyNewsroom ? <App /> : <LandingPage />}
   </StrictMode>,
 );
