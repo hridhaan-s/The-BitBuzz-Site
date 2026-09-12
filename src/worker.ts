1a3aee8bf1ff07cd9abcb1945e755de14944c702
Hridhaan-Sahay <study.hridhaan@gmail.com>
2026-09-12 12:12:03 +0530

    fix Worker TypeScript type for Cloudflare build

--- src/worker.ts
+++ src/worker.ts
@@ -1,5 +1,9 @@
+interface AssetFetcher {
+  fetch(request: Request): Promise<Response>;
+}
+
 export default {
-  async fetch(request: Request, env: { ASSETS: Fetcher }) {
+  async fetch(request: Request, env: { ASSETS: AssetFetcher }) {
     return env.ASSETS.fetch(request);
   },
 };
